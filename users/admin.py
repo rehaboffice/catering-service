ac244e53d7d62cdb9b0cdf59896e15130d0e6db9
rehaboffice <rehabtoffice@gmail.com>
2025-06-25 16:56:53 +0500

    `added users/admin.py`

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Profile
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'full_name', 'role', 'is_staff']
+    search_fields = ['email', 'full_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')}),
+        ('Groups & Permissions', {'fields': ('groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','full_name', 'role', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
